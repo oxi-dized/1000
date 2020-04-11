f28fc15182407e0a3de87ae0c5e253fd659435a9
oxi-dized <owidiux@gmail.com>
2020-04-11 21:17:48 +0200

    1.02
    -Player can play only a card of a proper type

--- Assets/Scripts/CardPicking.cs
+++ Assets/Scripts/CardPicking.cs
@@ -1,22 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CardPicking : MonoBehaviour
 {
+
+    private bool cardOfMasterType = false;
+
+
     private void OnMouseDown()
     {
-        //
-        
-        PlayerPicker playerPicker = GameObject.Find("Game Master").GetComponent<PlayerPicker>();
-        int parentID = transform.parent.gameObject.GetComponent<PlayerController>().playerID;
-        
 
-        if (playerPicker.masterPlayerID== parentID)
+        EndOfTurnCalculation endOfTurnCalculation = GameObject.Find("Game Master").GetComponent<EndOfTurnCalculation>();
+        int parentID = transform.parent.gameObject.GetComponent<PlayerController>().playerID;
+    
+        // Checking if it's this player's move
+        if (endOfTurnCalculation.masterPlayerID== parentID)
         {
-            transform.position = new Vector3(12, transform.position.y, transform.position.z);
-            playerPicker.masterPlayerID = playerPicker.masterPlayerID+1;
-            gameObject.tag = "CardInPlay";
-        }                
+            
+            // Checking if it's the first player in a round
+            if (parentID==0)
+            {
+                endOfTurnCalculation.masterCardType = GetComponent<CardProperties>().type;
+
+                cardMove(endOfTurnCalculation);
+            }
+            else
+            {
+                // Checking if there exists a card with of a proper type in a player's hand
+                foreach (Transform child in transform.parent)
+                {
+                    if (child.GetComponent<CardProperties>().type == endOfTurnCalculation.masterCardType)
+                    {
+                        cardOfMasterType = true;
+                    }
+                }
+
+                // Checink if the card is of a proper type, or if there isn't any card of a proper type in a player's hand
+                if (GetComponent<CardProperties>().type == endOfTurnCalculation.masterCardType || !cardOfMasterType)
+                {
+                    cardMove(endOfTurnCalculation);
+                }
+            }
+
+                                  
+        }
+    }
+
+    // Function moving the card
+    void cardMove(EndOfTurnCalculation endOfTurnCalculation)
+    {
+        // Moving the card
+        transform.position = new Vector3(12, transform.position.y, transform.position.z);
+        // Ending the player's move
+        endOfTurnCalculation.masterPlayerID = endOfTurnCalculation.masterPlayerID + 1;
+        // Taging the played card with proper tag
+        gameObject.tag = "CardInPlay";
     }
 }
---
+++ Assets/Scripts/EndOfTurnCalculation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EndOfTurnCalculation : MonoBehaviour
+{
+    // ID of a current player
+    public int masterPlayerID;
+    // Type of the first played card
+    public int masterCardType;
+
+    private GameObject[] cardsInPlay;
+    private GameObject[] players;
+    private int[] valuesOfCardsInPlay = new int[3];
+    private int[] typesOfCardsInPlay = new int[3];
+    private int[] weightedValuesOfCardsInPlay = new int[3];
+
+    private void Start()
+    {
+        // Player list
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (masterPlayerID == 3)
+        {           
+            // List of all the cards in play 
+            cardsInPlay = GameObject.FindGameObjectsWithTag("CardInPlay");
+            // Iterator
+            int i = 0;
+
+            // Cards in play analysis
+            foreach (GameObject card in cardsInPlay)
+            {
+                typesOfCardsInPlay[i] = Convert.ToInt32(card.GetComponent<CardProperties>().type == masterCardType);
+                // Array of values of cards in play
+                valuesOfCardsInPlay[i] = card.GetComponent<CardProperties>().value;
+                // Array of values of cards in play of a proper type
+                weightedValuesOfCardsInPlay[i] = valuesOfCardsInPlay[i] * typesOfCardsInPlay[i];
+                i++;
+                Destroy(card);
+            }
+            // Maximal value of cards in play
+            int maximalValue = weightedValuesOfCardsInPlay.Max();
+            // Player winnig this turn
+            int winningPlayer = Array.IndexOf(weightedValuesOfCardsInPlay, maximalValue);
+            // Adding values of all cards in this turn to the score of a winning player
+            cardsInPlay[winningPlayer].transform.parent.GetComponent<PlayerController>().score += valuesOfCardsInPlay.Sum();
+
+
+            //Determining the order of players in the next turn
+            foreach(GameObject player in players)
+            {
+                player.GetComponent<PlayerController>().playerID = (player.GetComponent<PlayerController>().playerID + 3 - winningPlayer) % 3;
+            }
+
+            masterPlayerID = 0;
+        }
+    }
+}
